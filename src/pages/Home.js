37c0b527bd6d9eef3a14f0c993d1ef7b7fa4bd1e
subarna <subarna769@gmail.com>
2022-02-08 00:20:37 +0600

    home page text add

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_bg">
+                <h1>This is Home page</h1>
+                
+            </div>
+        </div>
+    )
+}
+export default Home;
